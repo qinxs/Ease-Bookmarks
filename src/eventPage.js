7da036648441f6115fe87548f22d0bdf65672790
qinxs <qin_xs@sina.cn>
2022-01-24 18:34:17 +0800

    fix: 升级旧数据

--- src/eventPage.js
+++ src/eventPage.js
@@ -16,12 +16,30 @@ function checkRootInfo(argument) {
   });
 }
 
+function updataOldData() {
+  // v1.2.6 ↑
+  // 从上次位置启动 改为 从任意目录启动
+  chrome.storage.sync.get(null, results => {
+    var startup = results.startupFromLast;
+    if (startup) {
+      chrome.storage.sync.set({'startup': startup}, () => {});
+      chrome.storage.sync.remove('startupFromLast', () => {});
+      localStorage.setItem('startupID', localStorage.LastFolderID);
+      localStorage.removeItem('LastFolderID');
+    }
+  });
+}
+
 // 安装、更新
 chrome.runtime.onInstalled.addListener(() => {
   var manifest = chrome.runtime.getManifest();
   localStorage.setItem('version', manifest.version);
 
   checkRootInfo();
+
+  try {
+    updataOldData();
+  } catch {}
 });
 
 // chrome.runtime.onStartup.addListener(function() {
