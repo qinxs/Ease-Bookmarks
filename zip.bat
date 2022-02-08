fefa9a9fb10237b2187d31aee977ff56980063dd
qinxs <qin_xs@sina.cn>
2022-02-08 20:36:52 +0800

    chore: 打包脚本

---
+++ zip.bat
@@ -0,0 +1,36 @@
+color 0A
+title 打包为zip
+
+rem 指定WINRAR路径
+set "path=%path%;D:\Program Files\WinRAR"
+
+cd src
+:: dependencies
+:: npm install clean-css-cli -g
+:: npm install terser -g
+pushd css
+call cleancss -o popup.css popup.css
+popd
+pushd js
+call terser config.js -o config.js
+call terser popup.js -o popup.js
+popd
+
+cd..
+WinRAR a -r -ibck -inul -ep1 ^
+src.zip ^
+src\*
+
+:: -r :连同子文件夹
+:: -ibck :后台运行
+:: -inul :禁止错误信息
+:: -ep1 :从名称中排除主文件夹(src)
+
+cls
+echo 打包完成 请确认！！！
+echo.
+echo 按任意键还原被压缩的文件(js、css)...
+echo 否则 直接关闭退出
+pause>nul
+
+git checkout .
