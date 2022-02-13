fb84fd11aa19a70fc2db09aa7ee891a94c8f855d
qinxs <qin_xs@sina.cn>
2022-02-13 16:17:52 +0800

    chore: 打包脚本优化

--- zip.bat
+++ zip.bat
@@ -2,7 +2,10 @@ color 0A
 title 打包为zip
 
 rem 指定WINRAR路径
-set "path=%path%;D:\Program Files\WinRAR"
+@set "path=%path%;D:\Program Files\WinRAR"
+
+rem 检查是git状态
+for /f "delims=" %%i in ('git status -s') do set "status=%%i"
 
 cd src
 :: dependencies
@@ -27,10 +30,23 @@ src\*
 :: -ep1 :从名称中排除主文件夹(src)
 
 cls
-echo 打包完成 请确认！！！
+echo 打包完成 请测试确认！！！
 echo.
+
+if "%status%" == "" (
+  goto checkout
+) else (
+  goto exit
+)
+
+:checkout
 echo 按任意键还原被压缩的文件(js、css)...
 echo 否则 直接关闭退出
 pause>nul
-
 git checkout .
+exit
+
+:exit
+echo 由于打包前存在未提交文件
+echo 请手动还原相关被压缩文件...
+pause>nul
