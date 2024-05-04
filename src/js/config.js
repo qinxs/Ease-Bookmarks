983af89dd2ee5e391560f83c878e7495395bad05
qin_xs <qin_xs@qq.com>
2024-05-04 21:21:11 +0800

    style：修改内置参数的默认值
    keepMaxCols：0 -> 1

--- src/js/config.js
+++ src/js/config.js
@@ -23,7 +23,7 @@ window.BM = {
     fastCreate: 0, // 0-2
     hotkeyCancelSeleted: 'Space',
     hotkeyDelete: '-Delete',
-    keepMaxCols: 1, // 0-1 保持最大宽度
+    keepMaxCols: 0, // 0-1 保持最大宽度
     // 1-2
     // 1：仅url 
     // 2：url和title
