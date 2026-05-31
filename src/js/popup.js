2451cb4abf1258b57b9f06275816140e6257b52c
qin_xs <qin_xs@qq.com>
2026-05-31 17:35:09 +0800

    fix: 重置旧数据的其他书签ID
    其他书签ID与rootInfo中不一致时 重置rootInfo

--- src/js/popup.js
+++ src/js/popup.js
@@ -1527,6 +1527,12 @@ Promise.all([
   ]).then(() => {
   // console.log(BM.settings);
   settings = BM.settings;
+  // 其他书签ID变了
+  if (!settings.rootInfo[bookmarkNode.other]) {
+    chrome.runtime.sendMessage({ task: 'setRootInfo' }, () => {
+      location.reload();
+    });
+  }
   dataSetting.init();
 
   $curFolderList.id = `_${BM.startupReal}`;
